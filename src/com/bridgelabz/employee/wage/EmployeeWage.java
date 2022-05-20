724313cbc6d3558c71e28de4e8b1da00d5050a62
Vikash <vikashpathak041@gmail.com>
2022-05-21 00:49:01 +0530

    added welcome message

---
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employee.wage;
+
+import java.util.Random;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	
+	
+	}
+
+}
