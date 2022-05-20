7d1fc13b935d0abe144a00a48d05070a35f5ac7f
Vikash <vikashpathak041@gmail.com>
2022-05-21 01:06:32 +0530

    added part time wages

--- src/com/bridgelabz/employee/wage/EmployeeWage.java
+++ src/com/bridgelabz/employee/wage/EmployeeWage.java
@@ -8,18 +8,25 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(2);
+		int attendance = random.nextInt(3);
 
 		int wagePerHour = 20;
 		int fullDayHours = 8;
+		int partTimeHours = 4;
 		int wagePerDay = 0;
 
-		if (attendance == 1) {
-			System.out.println("Employee Present..");
-			wagePerDay = wagePerHour * fullDayHours;
-		} else
-			System.out.println("Employee Absent");
+		switch (attendance) {
+		case 0:
+			System.out.println("Employee Absent.");
+			break;
+		case 1:
+			wagePerDay = partTimeHours * wagePerHour;
+			break;
+		default:
+			wagePerDay = fullDayHours * wagePerHour;
 
+		}
+ 
 		System.out.println("Employee Wage Per Day: " + wagePerDay);
 
 	}
